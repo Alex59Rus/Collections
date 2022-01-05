9244b76e95ae8c2ad37bcce3b68e7f560258a1c6
Alex Oborin <cudlove.1@gmail.com>
2022-01-05 21:51:32 +0500

    1.1 first success for second task

--- logic.java
+++ logic.java
@@ -2,23 +2,33 @@ import java.util.*;
 
 public class main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        List<Integer> colCount = new ArrayList();
+        List<Integer> col2 = new ArrayList();
         List<Integer> col = new ArrayList();
-        col.add(3);
+        List<Integer> col1 = new ArrayList();
+        col.add(4);
+        col.add(5);
+        col.add(6);
+        col.add(6);
         col.add(8);
-        col.add(15);
-        col.add(17);
-        int input = scanner.nextInt();
-        for (int i = 0; i <= col.size(); i++) {
-            int counter = input - col.get(i);
-            //System.out.println(counter);
-            if (col.contains(counter)) {
-                if (true){
-                    colCount.add(col.indexOf(counter));
-                    System.out.println("result " + colCount);
+        col2.add(4);
+        col2.add(5);
+        col2.add(6);
+        col2.add(7);
+        col2.add(8);
+        for (int i = 0; i < col.size(); i++) {
+            for (int j = i+1; j < col.size(); j++) {
+                if (col.get(i) == col.get(j)){
+                    col1.add(col.get(i));
                 }
             }
+        }
+        if (col1 != null){
+            System.out.println("Result: true");
+            System.out.println("Число " + col1 + " повторяется 2 раза");
+        }
+        if (col1 == null) {
+            System.out.println("Result: false");
+            System.out.println("Дубликатов нет");
         }
     }
 }
