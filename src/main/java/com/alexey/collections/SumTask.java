f09effa29f9c0f6d9ca9e916f008db1e92e5d2c3
Alex Oborin <cudlove.1@gmail.com>
2022-01-16 14:17:23 +0500

    Collections practice third iteration/add first task

--- src/main/java/com/alexey/collections/SumTask.java
+++ src/main/java/com/alexey/collections/SumTask.java
@@ -3,8 +3,14 @@ package com.alexey.collections;
 import java.util.*;
 
 public class SumTask {
-    public List<Integer> checkSum(List<Integer> numbers, Integer sum) {
-        //NOT n^2
-        return null;
+    public static List<Integer> checkSum(List<Integer> numbers, Integer sum) {
+        List<Integer> colIndex = new ArrayList(); //складываем в эту коллекцию индексы у искомых чисел
+        for (int i = 0; i <= numbers.size() -1 ; i++) {
+            int desiredNumber = sum - numbers.get(i);
+            if (numbers.contains(desiredNumber)) {
+                colIndex.add(numbers.indexOf(desiredNumber));
+            }
+        }
+            return null;
     }
 }
