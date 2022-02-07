27438bee8ec433f8c67da234901e1936d99808e4
Alex Oborin <cudlove.1@gmail.com>
2022-02-07 15:14:11 +0500

    Collections practice third iteration/add corrections

--- src/main/java/com/alexey/collections/SumDemo.java
+++ src/main/java/com/alexey/collections/SumDemo.java
@@ -1,11 +1,26 @@
 package com.alexey.collections;
 
+import java.awt.image.AreaAveragingScaleFilter;
 import java.util.*;
 
 
 public class SumDemo {
     public static void main(String[] args) {
-        SumTask logic = new SumTask();
-        List<Integer> result = SumTask.checkSum(List.of(3,8,15,17), 23);
+        Scanner scanner = new Scanner(System.in);
+        int intUser = scanner.nextInt();
+        Map<Integer, Integer> numbers = new HashMap<>();
+        ArrayList<Integer> valueStore = new ArrayList<>();
+        int input = 23;
+        numbers.put(0, 3);
+        numbers.put(1, 8);
+        numbers.put(2, 15);
+        numbers.put(3, 17);
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            int x = input - numbers.get(i);
+            if (numbers.containsValue(x)) {
+                valueStore.add(i);
+            }
+        }
+        System.out.println("result: " + valueStore);
     }
 }
--- src/main/java/com/alexey/collections/UniqueDemo.java
+++ src/main/java/com/alexey/collections/UniqueDemo.java
@@ -1,12 +1,34 @@
 package com.alexey.collections;
 
-import java.util.List;
+import com.sun.source.tree.Tree;
+
+import java.util.*;
 
 public class UniqueDemo {
     public static void main(String[] args) {
-        UniqueTask task = new UniqueTask();
-        //List<Integer> result = UniqueTask.containsDuplicate(List.of(3,66,33,12,15,21,1,100,12));
-        List<Integer> result = UniqueTask.containsDuplicate(List.of(2,3,4,5,5,6,7,8));
-
+        ArrayList<Integer> arrayList = new ArrayList<>();
+        ArrayList<Integer> store = new ArrayList<>();
+        arrayList.add(1);
+        arrayList.add(4);
+        arrayList.add(5);
+        arrayList.add(6);
+        arrayList.add(19);
+        arrayList.add(4);
+        arrayList.add(7);
+        arrayList.add(7);
+        arrayList.add(7);
+        Collections.sort(arrayList);
+        Set<Integer> TreeSet = new TreeSet<>(arrayList);
+/*        if (arrayList.size() == TreeSet.size()) {
+            System.out.println("Дубликатов нет " + false);
+        }else {
+            System.out.println("Дубликаты есть " + true);
+        };*/
+        for (int i = 0; i < arrayList.size() - 1; i++) {
+            if (arrayList.get(i) == arrayList.get(i + 1)) {
+                store.add(arrayList.get(i));
+            }
+        }
+        System.out.println("Дубликаты " + store);
     }
 }
