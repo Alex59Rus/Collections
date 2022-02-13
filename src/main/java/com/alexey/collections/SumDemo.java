2149c92099d46a758628b8ec255e9f044584bc78
Alex Oborin <cudlove.1@gmail.com>
2022-02-13 14:49:23 +0500

    fix uniq task

--- src/main/java/com/alexey/collections/SumDemo.java
+++ src/main/java/com/alexey/collections/SumDemo.java
@@ -7,20 +7,16 @@ import java.util.*;
 public class SumDemo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int intUser = scanner.nextInt();
+        System.out.print("Введите число: ");
+        int desired = scanner.nextInt();
+
         Map<Integer, Integer> numbers = new HashMap<>();
-        ArrayList<Integer> valueStore = new ArrayList<>();
-        int input = 23;
-        numbers.put(0, 3);
-        numbers.put(1, 8);
-        numbers.put(2, 15);
-        numbers.put(3, 17);
-        for (int i = 0; i < numbers.size() - 1; i++) {
-            int x = input - numbers.get(i);
-            if (numbers.containsValue(x)) {
-                valueStore.add(i);
-            }
-        }
-        System.out.println("result: " + valueStore);
+        Map<Integer,Integer> mapa = Map.of(0,3,1,8,2,15,3,17);
+
+        System.out.println("result: " + mapa);
+    }
+
+    private static HashMap<Integer, Integer> findSum(HashMap<Integer,Integer> input, Integer desired) {
+        return null;
     }
 }
--- src/main/java/com/alexey/collections/UniqueDemo.java
+++ src/main/java/com/alexey/collections/UniqueDemo.java
@@ -4,31 +4,19 @@ import com.sun.source.tree.Tree;
 
 import java.util.*;
 
+import static java.util.List.of;
+
 public class UniqueDemo {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        ArrayList<Integer> store = new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(4);
-        arrayList.add(5);
-        arrayList.add(6);
-        arrayList.add(19);
-        arrayList.add(4);
-        arrayList.add(7);
-        arrayList.add(7);
-        arrayList.add(7);
-        Collections.sort(arrayList);
-        Set<Integer> TreeSet = new TreeSet<>(arrayList);
-/*        if (arrayList.size() == TreeSet.size()) {
+        List<Integer> arrayList = definition(List.of(1,2,3,4,5,6));
+    }
+    private static ArrayList<Integer> definition(List<Integer> arrayList) {
+        Set<Integer> listUn = new TreeSet<>(arrayList);
+        if (arrayList.size() == listUn.size()) {
             System.out.println("Дубликатов нет " + false);
         }else {
             System.out.println("Дубликаты есть " + true);
-        };*/
-        for (int i = 0; i < arrayList.size() - 1; i++) {
-            if (arrayList.get(i) == arrayList.get(i + 1)) {
-                store.add(arrayList.get(i));
-            }
         }
-        System.out.println("Дубликаты " + store);
+        return null;
     }
 }
